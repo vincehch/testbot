3cf5a9bf46f7e3ef24115294266c7e713ce4b339
vincehch <35246356+vincehch@users.noreply.github.com>
2018-01-09 14:01:18 +0800

    Create receive.php

---
+++ receive.php
@@ -0,0 +1,29 @@
+<?php
+ $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
+ $json_obj = json_decode($json_str); //轉JSON格式
+ //產生回傳給line server的格式
+ $sender_userid = $json_obj->events[0]->source->userId;
+ $sender_txt = $json_obj->events[0]->message->text;
+ $response = array (
+				"to" => $sender_userid,
+				"messages" => array (
+					array (
+						"type" => "text",
+						"text" => "Hello, YOU SAY ".$sender_txt
+					)
+				)
+		);
+ $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
+ fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+ fclose($myfile);
+ //回傳給line server
+ $header[] = "Content-Type: application/json";
+ $header[] = "Authorization: Bearer B6qIEPkUfRg0n304IHX/wtuFQ4S2Nga/ZZJC4o4Ej0nofTSA9O0Yn/BOWP/2zubHV8xiSmpcStJc8ELXckGit/BG/ThWsUQbNW0Mfu2/NOBni9+QlIhBjWoaXSRyIMPjRMQ+BpwmH7vpGt8TTL541AdB04t89/1O/w1cDnyilFU=";
+ $ch = curl_init("https://api.line.me/v2/bot/message/push");                                                                      
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
+ $result = curl_exec($ch);
+ curl_close($ch); 
+?>
